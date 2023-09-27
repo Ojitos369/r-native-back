02f0342d59615e70bb6ebf7317bb8d0ed0d3b655
ojitos369 <ojitos369@gmail.com>
2023-09-27 13:04:34 -0600

    update ignore

--- app/settings.py
+++ app/settings.py
@@ -174,3 +174,5 @@ ce = CE(name_project = 'REANGO BASE', email_settings = email_settings)
 
 class MYE(Exception):
     pass
+
+# ./ddp.sh --name "rn-test" --repo "git@github.com:Ojitos369/r-native-back.git" --url "rn-test.ojitos369.com"
